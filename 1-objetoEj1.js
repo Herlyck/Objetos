2c3be925abab99f69f7d92916b9bcf63d5da0a83
Herlyck <mrcyrax64@gmail.com>
2026-06-05 20:45:48 -0300

    prueba

--- 1-objetoEj1.js
+++ 1-objetoEj1.js
@@ -17,36 +17,30 @@ function main() {
     let dia = 0;
 
 
-    // let tarea = {
-    //     tarea: "lavar ropa",
-    //     fecha: "02/03/2025",
-    //     estado:SIN_TERMINAR,
-    //     estado1:TERMINADO
-    // };
+
 
     console.log("seleccione dia 0-6");
     dia = Number(leer());
     console.log(DIAS[dia]);
 
 
-    // for (let i = 0; i <1 ; i++) {
-    //     matriz.push([]);
-    //     for (j = 0; j < 6; j++){
-    //         matriz[i].push(tarea);
-    //     }
+    for (let i = 0; i <1 ; i++) {
+        matriz.push([{}]);
+        for (j = 0; j < 6; j++){
+           
+            matriz[dia][0].tarea;
+            matriz[dia][0].fecha;
+            matriz[dia][0].estado;
+    
+        }
 
-    // }
-    // console.log(tarea.tarea);
-    // console.log(tarea.fecha);
+    }
+    console.log(tarea.tarea);
+    console.log(tarea.fecha);
 
-    console.log(matriz[dia][0].tarea);
-    console.log(matriz[dia][0].fecha);
-    console.log(matriz[dia][0].estado);
+   
     
 
 }
 main();
 
-if (condition) {
-    
-};
