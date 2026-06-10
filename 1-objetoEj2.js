85701cfe54526508402266056bf5203fbf21555d
Herlyck <mrcyrax64@gmail.com>
2026-06-10 18:38:07 -0300

    actualizado

--- 1-objetoEj2.js
+++ 1-objetoEj2.js
@@ -1,43 +1,66 @@
 const leer = require("prompt-sync")();
 const DIAS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 const TAREAS = ["lavar ropa", "cocinar", "limpiar casa", "planchar", "Programar", "Barrer", "acomodar"];
-const SIN_TERMINAR="sin terminar";
-const TERMINADO= "Terminado";
+const SIN_TERMINAR = "sin terminar";
+const TERMINADO = "Terminado";
 function main() {
 
     let matriz = [
-        [{ tarea: "lavar ropa", fecha: "02/03/2025",estado:SIN_TERMINAR }], //0
-        [{ tarea: "cocinar", fecha: "16/02/2026",estado:TERMINADO }],//1
+    [{ tarea: "lavar ropa", fecha: "02/03/2025", estado: SIN_TERMINAR }], //0
+        [{ tarea: "cocinar", fecha: "16/02/2026", estado: TERMINADO }],//1
         [],
         [],
         [],
         [],
-        [],
-        [],
+        []
     ];
-    let dia = 0;
-    let nuevaTarea=0;
 
+    let opcion = 0;
+    let nuevaTarea = 0;
+    let cantTareas = 0;
+    let nuevasTareas={};
 
+    console.log("cantidad de tareas:");
+    cantTareas = Number(leer());
 
-    console.log("seleccione dia 0-6");
-    dia = Number(leer());
-    console.log(DIAS[dia]);
+    for (let i = 0; i < cantTareas; i++) {
+        console.log("seleccione dia 0-6");
+        opcion = Number(leer());
+        console.log(DIAS[opcion]);
 
-    console.log("agregue nueva tarea:")
-    nuevaTarea=leer(); //comer
-    matriz[dia].tarea1 = nuevaTarea; 
-    console.log("------------------");
-    
-    
+        console.log("agregue nueva tarea:")
+        nuevaTarea = leer(); 
 
-   
-    console.log(matriz[dia][0].tarea);
-    console.log(matriz[dia][0].fecha);
-    console.log(matriz[dia][0].estado);
-    
-    
+        nuevasTareas = {
+            tarea: nuevaTarea,
+            fecha: "10/06/2026", 
+            estado: SIN_TERMINAR
+        };
+        matriz[opcion].push(nuevasTareas);
+        
+    }
+    console.log(matriz);
+    console.log(matriz[opcion][0].fecha)
+
+    // matriz[opcion][0].fecha
+    // nuevaTareas.fecha
 
 }
 main();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
