2303fd1c8be4d45821b2b7588ba8ef50e27ab940
Herlyck <mrcyrax64@gmail.com>
2026-06-10 20:07:14 -0300

    ej 3 terminado

--- 3-objetoEj3.js
+++ 3-objetoEj3.js
@@ -5,41 +5,40 @@ function main() {
     const DIAS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     const SIN_TERMINAR = "sin terminar";
     const TERMINADO = "Terminado";
-   
 
-        let tareas = [
-            [{ tarea: "lavar ropa", fecha: "02/03/2025", estado: SIN_TERMINAR}, {tarea: "jugar con mascota", fecha: "02/03/2025", estado: SIN_TERMINAR }], //0
-            [],
-            [{ tarea: "ordenar cuarto",fecha: "02/03/2025", estado: SIN_TERMINAR}, {tarea: "hacer ejercicio",fecha: "02/03/2025", estado: SIN_TERMINAR}],
-            [],
-            [],
-            []
-        ];
-        
-        let opcion = 0;
-        let elegirTarea=0;
-         let propiedadEliminar = 0;
-        let tareasEliminar = 0;
-        let nuevasTareas = {};
 
-        console.log("seleccione un dia 0-6");
-        opcion= Number(leer());
+    let tareas = [
+        [{ tarea: "lavar ropa", fecha: "02/03/2025", estado: SIN_TERMINAR }, { tarea: "jugar con mascota", fecha: "02/03/2025", estado: SIN_TERMINAR }], //0
+        [],
+        [{ tarea: "ordenar cuarto", fecha: "02/03/2025", estado: SIN_TERMINAR }, { tarea: "hacer ejercicio", fecha: "02/03/2025", estado: SIN_TERMINAR }],
+        [],
+        [],
+        []
+    ];
 
-        console.log("mostrando tareas del dia:",DIAS[opcion])
-        console.log("1-",tareas[opcion][0].tarea);
-        console.log("2-",tareas[opcion][1].tarea);
+    let opcion = 0;
+    let elegirTarea = 0;
+    let propiedadEliminar = 0;
+    
 
-        console.log("seleccione una tarea:")
-        elegirTarea=Number(leer())-1;
+    console.log("seleccione un dia 0-6");
+    opcion = Number(leer());
 
-        console.log("propiedades en ",tareas[opcion][elegirTarea].tarea, tareas[opcion][elegirTarea]);
+    console.log("mostrando tareas del dia:", DIAS[opcion])
+    console.log("1-", tareas[opcion][0].tarea);
+    console.log("2-", tareas[opcion][1].tarea);
 
-        console.log("que propiedad deseas eliminar?");
-        propiedadEliminar = leer();
+    console.log("seleccione una tarea:")
+    elegirTarea = Number(leer()) - 1;
 
-        delete tareas[opcion][elegirTarea][propiedadEliminar];
-        console.log(tareas);
-        
+    console.log("propiedades en ", tareas[opcion][elegirTarea].tarea, tareas[opcion][elegirTarea]);
+
+    console.log("que propiedad deseas eliminar?");
+    propiedadEliminar = leer();
+
+    delete tareas[opcion][elegirTarea][propiedadEliminar];
+    console.log(tareas);
+
 
 }
 main();
